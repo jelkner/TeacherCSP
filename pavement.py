3af04556bf9c247dec428e92ae32420ec738747e
ericsonga <ericson@cc.gatech.edu>
2017-06-19 10:15:01 +0200

    Change to the directory
    Change to the serving directory

--- pavement.py
+++ pavement.py
@@ -20,7 +20,7 @@ if master_url is None:
         master_url = 'http://127.0.0.1:8000'
 
 master_app = 'runestone'
-serving_dir = "./build/TeacherCSP"
+serving_dir = "./build/TeacherCSP-Ex2a"
 
 options(
     sphinx = Bunch(docroot=".",),
